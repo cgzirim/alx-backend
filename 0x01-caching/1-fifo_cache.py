216dfe461b344cef552f3a16f46ca474e1bed931
Chigozirim <iamokwukwe@gmail.com>
2022-07-13 00:00:02 +0100

    Do task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -2,7 +2,7 @@
 """Defines a class FIFOCache that inherits from BaseCaching."""
 from collections import OrderedDict
 
-BaseCaching = __import__('base_caching').BaseCaching
+BaseCaching = __import__("base_caching").BaseCaching
 
 
 class FIFOCache(BaseCaching):
@@ -17,16 +17,15 @@ class FIFOCache(BaseCaching):
         If key or item is None, this method does nothing.
         """
         if key is None or item is None:
-            return 
+            return
 
         self.cache_data.update({key: item})
 
         if len(self.cache_data) > self.MAX_ITEMS:
             ord_dict = OrderedDict(self.cache_data)
-            k_v = ord_dict.popitem(last = False)
+            k_v = ord_dict.popitem(last=False)
             del self.cache_data[k_v[0]]
             print("DISCARD: {}".format(k_v[0]))
-            
 
     def get(self, key):
         """Returns the value in self.cache_data linked to key."""
