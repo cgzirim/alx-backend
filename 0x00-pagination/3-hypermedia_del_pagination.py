c12d676790e2b502fb9d46bab857734a3ecfdb49
Chigozirim <iamokwukwe@gmail.com>
2022-07-15 14:56:20 +0100

    Align text to PEP8 standard

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -9,8 +9,8 @@ from typing import List, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -18,8 +18,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -29,44 +28,41 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
+        """Dataset indexed by sorting position, starting at 0"""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
+            self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            """ Deletion-resilient hypermedia pagination """
+        """Deletion-resilient hypermedia pagination"""
 
-            idx_dataset = self.indexed_dataset()
+        idx_dataset = self.indexed_dataset()
 
-            assert isinstance(index, int) and index < (len(idx_dataset) - 1)
+        assert isinstance(index, int) and index < (len(idx_dataset) - 1)
 
-            i, mv, data = 0, index, []
-            while (i < page_size and index < len(idx_dataset)):
-                value = idx_dataset.get(mv, None)
-                if value:
-                    data.append(value)
-                    i += 1
-                mv += 1
+        i, mv, data = 0, index, []
+        while i < page_size and index < len(idx_dataset):
+            value = idx_dataset.get(mv, None)
+            if value:
+                data.append(value)
+                i += 1
+            mv += 1
 
-            next_index = None
-            while (mv < len(idx_dataset)):
-                value = idx_dataset.get(mv, None)
-                if value:
-                    next_index = mv
-                    break
-                mv += 1
+        next_index = None
+        while mv < len(idx_dataset):
+            value = idx_dataset.get(mv, None)
+            if value:
+                next_index = mv
+                break
+            mv += 1
 
-            hyper = {
-                'index': index,
-                'next_index': next_index,
-                'page_size': page_size,
-                'data': data
-            }
+        hyper = {
+            "index": index,
+            "next_index": next_index,
+            "page_size": page_size,
+            "data": data,
+        }
 
-            return hyper
+        return hyper
