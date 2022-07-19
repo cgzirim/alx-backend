b0fa3190222bd09efdc2c4df7021e0f9ec0588a0
Chigozirim <iamokwukwe@gmail.com>
2022-07-19 17:05:54 +0100

    Add docstring to get_locale function

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,6 +22,7 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    """Gets the best match from supported languages."""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
