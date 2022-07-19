8a56c927557ca2d564b1918c2de297eb58dd5a68
Chigozirim <iamokwukwe@gmail.com>
2022-07-19 17:43:31 +0100

    Make get_locale typed

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -22,9 +22,9 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Gets the best match from supported languages."""
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route("/")
