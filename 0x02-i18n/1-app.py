62a344633334dd7cb4c19f4aa1fc63f60f77e416
Chigozirim <iamokwukwe@gmail.com>
2022-07-19 16:13:12 +0100

    Set up Babel in app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_babel import Babel
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 babel = Babel(app)
 
 
