bdf9c127a9c65d2c88c1dee46b45a74b2002657c
Chigozirim <iamokwukwe@gmail.com>
2022-07-19 16:06:52 +0100

    Set up Babel in app

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Sets up basic Flask app."""
+from flask import Flask
+from flask_babel import Babel
+from flask import render_template
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """Class to configure available languages."""
+
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.form_object(Config)
+
+
+@app.route("/")
+def index():
+    """View function for route /."""
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
